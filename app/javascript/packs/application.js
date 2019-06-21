da1380268ba1e3b7b74ebd2c268cef38c78c2bf4
Georg Ledermann <mail@georg-ledermann.de>
2019-06-21 11:30:05 +0200

    Add Vue, TailwindCSS, PurgeCSS, Webpacker config

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+import '../styles/application.scss'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -17,3 +18,17 @@ require("channels")
 // const imagePath = (name) => images(name, true)
 
 import "controllers"
+
+// Vue
+import TurbolinksAdapter from 'vue-turbolinks'
+import Vue from 'vue'
+
+Vue.use(TurbolinksAdapter)
+
+document.addEventListener('turbolinks:load', () => {
+  for (const element of document.querySelectorAll('[data-behavior="vue"]')) {
+    const app = new Vue({
+      el: element
+    })
+  }
+});
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,22 @@
 const { environment } = require('@rails/webpacker')
+const webpack = require('webpack')
+const { VueLoaderPlugin } = require('vue-loader')
+const vue = require('./loaders/vue')
 
+const path = require('path')
+environment.config.merge({
+  resolve: {
+    alias: {
+      '@': path.resolve('app/javascript'),
+      vue$: 'vue/dist/vue.esm.js',
+    }
+  }
+})
+
+environment.plugins.prepend('Provide', new webpack.ProvidePlugin({
+  Rails: ['@rails/ujs']
+}))
+
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.prepend('vue', vue)
 module.exports = environment

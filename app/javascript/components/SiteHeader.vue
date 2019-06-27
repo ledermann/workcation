437143aaaa1a647beadf5537a3b19b7f5a326379
Georg Ledermann <mail@georg-ledermann.de>
2019-06-27 07:25:49 +0200

    Search inputs: Use type "search" to add clear button

--- app/javascript/components/SiteHeader.vue
+++ app/javascript/components/SiteHeader.vue
@@ -18,8 +18,8 @@
         <div class="absolute inset-y-0 left-0 flex items-center pl-3">
           <svg class="h-6 w-6 fill-current text-gray-600" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M16.32 14.9l1.1 1.1c.4-.02.83.13 1.14.44l3 3a1.5 1.5 0 0 1-2.12 2.12l-3-3a1.5 1.5 0 0 1-.44-1.14l-1.1-1.1a8 8 0 1 1 1.41-1.41l.01-.01zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"/></svg>
         </div>
-        <form>
-          <input class="block w-full form-input form-input-light pl-10 pr-4 py-2" placeholder="Search by keywords" name="search" :value="searchText">
+        <form action=".">
+          <input type="search" class="block w-full form-input form-input-light pl-10 pr-4 py-2" placeholder="Search by keywords" name="search" :value="searchText" id="header-search">
         </form>
       </div>
       <div class="sm:flex sm:items-center">

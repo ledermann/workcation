7bd9e9ef007cfff8d93e2381fd4bdd4761b9707f
Georg Ledermann <mail@georg-ledermann.de>
2019-11-09 13:23:31 +0100

    Prepare database, add ActiveRecord models

---
+++ app/models/property.rb
@@ -0,0 +1,25 @@
+class Property < ApplicationRecord
+  belongs_to :location
+
+  scope :search_for_keywords, lambda { |keywords|
+    if keywords.present?
+      keywords.split(' ').reduce(self) do |result, word|
+        result.where('title ILIKE ?', "%#{sanitize_sql_like word.strip}%")
+      end
+    else
+      all
+    end
+  }
+
+  scope :search_for_beds, lambda { |min|
+    min ? where("beds >= ?", min.to_i) : all
+  }
+
+  scope :search_for_baths, lambda { |min|
+    min ? where("baths >= ?", min.to_i) : all
+  }
+
+  scope :search_for_price, lambda { |max|
+    max ? where("price <= ?", max.to_i) : all
+  }
+end

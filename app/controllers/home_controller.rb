5042cb5b1f27aea8eda7765ab10756fa73406935
Georg Ledermann <georg@ledermann.dev>
2022-04-21 21:02:53 +0200

    :arrow_up: Upgrade gem "rubocop" to 1.28.1

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,9 @@ class HomeController < ApplicationController
   def index
     render inertia: 'Home', props: {
       git: {
-        commit_time: ENV['COMMIT_TIME'] || Time.current.to_s,
+        commit_time: ENV.fetch('COMMIT_TIME', nil) || Time.current.to_s,
         commit_sha:  ENV['COMMIT_SHA'] ? ENV['COMMIT_SHA'].first(7) : 'development',
-        commit_url:  ENV['COMMIT_SHA'] ? "https://github.com/ledermann/workcation/commit/#{ENV['COMMIT_SHA']}" : 'https://github.com/ledermann/workcation'
+        commit_url:  ENV['COMMIT_SHA'] ? "https://github.com/ledermann/workcation/commit/#{ENV.fetch('COMMIT_SHA', nil)}" : 'https://github.com/ledermann/workcation'
       }
     }
   end
